baf29a4c0c9f4c799bbe64ce3ce337b61b452d5d
Michael <mrmichaelgraver@gmail.com>
2017-10-11 18:59:52 -0600

    Added code to the timestamp class to get the time.  Started code to find the difference of time between the 2 dates but still needs some fixes.

--- ServiceFundraiserApp/TimeStamp.swift
+++ ServiceFundraiserApp/TimeStamp.swift
@@ -8,5 +8,33 @@
 
 import Foundation
 class timeStamp{
+    private var clockIn:Date
+    private var clockOut:Date
     
+    init(startTime:Date, finishTime:Date) {
+        clockIn = startTime
+        clockOut = finishTime
+    }
+    
+    func getStartTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        let timeString = formatter.string(from: clockIn)
+        return timeString
+    }
+    
+    func getFinishTime() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "h:mm a"
+        let timeString = formatter.string(from: clockOut)
+        return timeString
+    }
+    
+    func getDifference() -> String {
+        //TODO: Fix this funtion.
+        let cal = NSCalendar.current
+        let hour = cal.dateComponents([.hour], from: clockIn, to: clockOut)
+        let min = cal.dateComponents([.minute], from: clockIn, to: clockOut)
+        return "\(hour):\(min)"
+    }
 }
