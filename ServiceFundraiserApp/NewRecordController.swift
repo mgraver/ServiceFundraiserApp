b637bbf734d3b9033400c4d9476e022e5f92fc2c
Michael <mrmichaelgraver@gmail.com>
2017-10-17 14:36:50 -0600

    Added Files for the Views I am making

---
+++ ServiceFundraiserApp/NewRecordController.swift
@@ -0,0 +1,12 @@
+//
+//  NewRecordController.swift
+//  ServiceFundraiserApp
+//
+//  Created by Michael Graver on 10/17/17.
+//  Copyright © 2017 GL. All rights reserved.
+//
+
+import UIKit
+class NewRecordController: UIViewController {
+    
+}
