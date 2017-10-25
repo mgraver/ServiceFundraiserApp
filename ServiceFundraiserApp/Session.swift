891c40a57e34d8809de1a9d800a1634f2c2b2aec
nnlamb25 <nnlamb25@gmail.com>
2017-10-24 18:22:01 -0600

    Made the session class

---
+++ ServiceFundraiserApp/Session.swift
@@ -0,0 +1,28 @@
+//
+//  Session.swift
+//  ServiceFundraiserApp
+//
+//  Created by Nathan Lamb on 10/24/17.
+//  Copyright © 2017 GL. All rights reserved.
+//
+
+import Foundation
+
+class Session
+{
+    var name:String
+    var description:String
+    var totalIncome:Double
+    var totalHours:Double
+    var totalParticiapnts:Int
+    //var participantsArray = [Worker]()
+    
+    init(SessionTitle: String, Description: String)
+    {
+        name = SessionTitle
+        description = Description
+        totalIncome = 0
+        totalHours = 0
+        totalParticiapnts = 0
+    }
+}
