ede4e0bf932e69bb68041e274f3892358df477d9
Michael <mrmichaelgraver@gmail.com>
2017-11-02 10:37:19 -0600

    Added code to singleton to save the session data.

--- ServiceFundraiserApp/Singleton.swift
+++ ServiceFundraiserApp/Singleton.swift
@@ -7,14 +7,34 @@
 //
 
 import Foundation
-
-final class Singleton
+import os.log
+final class Singleton: NSObject, NSCoding
 {
     static let sharedInstance = Singleton()
     var sessions: [Session]
     
-    private init()
+    private override init()
     {
         sessions = []
+    }
+    
+    private init(sessions:[Session]) {
+        self.sessions = sessions
+    }
+    
+    
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(sessions, forKey: "sessionsArray")
+    }
+    
+    required convenience init?(coder aDecoder: NSCoder) {
+        guard let savedSessions = aDecoder.decodeObject(forKey: "sessionsArray") as? [Session]
+            else {
+                os_log("Failed to load sessiosn into singleton", log: OSLog.default, type: .debug)
+                return nil
+        }
+        
+        self.init(sessions: savedSessions)
     }
 }
