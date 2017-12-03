f3ee28e73c1156da6e9be5b229a607756761afce
Michael <mrmichaelgraver@gmail.com>
2017-12-02 19:21:20 -0700

    Added a folder with the files to do custom transitions.

---
+++ ServiceFundraiserApp/Present/SlideInPresentationManager.swift
@@ -0,0 +1,43 @@
+//
+//  SlideInPresentationManager.swift
+//  MedalCount
+//
+//  Created by Michael Graver on 10/26/17.
+//  Copyright © 2017 Ron Kliffer. All rights reserved.
+//
+
+import UIKit
+
+enum PresentationDirection {
+  case left
+  case top
+  case right
+  case bottom
+}
+
+class SlideInPresentationManager: NSObject {
+  var direction = PresentationDirection.left
+}
+
+extension SlideInPresentationManager: UIViewControllerTransitioningDelegate{
+  func presentationController(forPresented presented: UIViewController,
+                              presenting: UIViewController?,
+                              source: UIViewController) -> UIPresentationController? {
+    let presentationController = SlideInPresentationController(presentedViewController: presented,
+                                                               presenting: presenting,
+                                                               direction: direction)
+    return presentationController
+  }
+  
+  func animationController(forPresented presented: UIViewController,
+                           presenting: UIViewController,
+                           source: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+    return SlideInPresentationAnimator(direction: direction, isPresentation: true)
+  }
+  
+  func animationController(forDismissed dismissed: UIViewController)
+    -> UIViewControllerAnimatedTransitioning? {
+      return SlideInPresentationAnimator(direction: direction, isPresentation: false)
+  }
+ 
+}
